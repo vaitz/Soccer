b142334f74795a9367d994450f5eeb1e7ad03153
vaitz <vaitz.post.bgu.ac.il>
2021-05-25 18:37:59 +0300

    Add User Entity And Register Logic

--- domain-usecases/RefereeRegister.js
+++ domain-usecases/RefereeRegister.js
@@ -1,24 +1,35 @@
-const soccerDB = require('../data-access/soccerDB')
+const soccerDB = require('../data-access/SoccerDB');
+const bcrypt = require("bcrypt");
 
 
 
 async function register(userName, password, firstName, lastName, refType){
     // check fields exists
     if(!userName || !password || !firstName || !lastName || !refType){
-        return false;
+        return "Missing fields, make sure you entered the following: userName, password, firstName, lastName, refType.";
     }
 
+    if(password.length < 6 || !(/\d/.test(password) && /[a-zA-Z]/.test(password))){
+        return "The password entered is not according to the rules: more then 6 characters or equal, contains at least one letter and one digit.";
+    }
+
+    let hash_password = bcrypt.hashSync(
+        password,
+        parseInt(process.env.bcrypt_saltRounds)
+    );
+
     // check if referee user exists 
-    let exists = await soccerDB.findByUserName(userName);
+    let exists = await soccerDB.findUserByUserName(userName);
     if(exists){
-        console.log('Referee already exists in the DB');
-        return false;
+        console.log('User already exists in the DB');
+        return "User already exists in the DB.";
     }
 
     // add referee to DB
-    await soccerDB.insertRefereeUser(userName, password, firstName, lastName, refType);
+    await soccerDB.insertRefereeUser(userName, hash_password, firstName, lastName, refType);
+    await soccerDB.insertUser(userName, hash_password);
     console.log('Referee added to the DB');
-    return true;
+    return "Referee added to the DB";
     
 
 }
