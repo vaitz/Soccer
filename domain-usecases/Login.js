c3af18fc4593167d2b6548b2e8742fdaa4051ad0
vaitz <vaitz.post.bgu.ac.il>
2021-06-08 11:01:56 +0300

    regression test 1

--- domain-usecases/Login.js
+++ domain-usecases/Login.js
@@ -2,28 +2,29 @@ const soccerDB = require('../data-access/SoccerDB');
 const bcrypt = require("bcrypt");
 
 async function login(userName, password){
-    // check fields exists
+    
     try {
-
+        // // regression test 1
         // return "regression test try1";
-    if(!userName || !password){
-        return "Missing fields, make sure you entered the following: userName, password.";
-    }
+        // check fields exists
+        if(!userName || !password){
+            return "Missing fields, make sure you entered the following: userName, password.";
+        }
 
-    // check if user exists 
-    let exists = await soccerDB.findUserByUserName(userName);
-    if(!exists){
-        console.log('User not exists in the DB');
-        return "User not exists in the DB.";
-    }
+        // check if user exists 
+        let exists = await soccerDB.findUserByUserName(userName);
+        if(!exists){
+            console.log('User not exists in the DB');
+            return "User not exists in the DB.";
+        }
 
-    // getting the password that in the DB and compare it to the password inserted by the user
-    let passwordDB = await soccerDB.getPasswordByUserName(userName);
-    if(!bcrypt.compareSync(password, passwordDB)){
-        console.log("Username or Password incorrect");
-        return "Username or Password incorrect";
-    }else{
-        return "200, User login successfully.";
+        // getting the password that in the DB and compare it to the password inserted by the user
+        let passwordDB = await soccerDB.getPasswordByUserName(userName);
+        if(!bcrypt.compareSync(password, passwordDB)){
+            console.log("Username or Password incorrect");
+            return "Username or Password incorrect";
+        }else{
+            return "200, User login successfully.";
         }
         
     } catch (error) {
