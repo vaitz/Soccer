800347664312abd727517130f16e0641843824ef
vaitz <vaitz.post.bgu.ac.il>
2021-06-09 09:27:45 +0300

    Add try catch

--- domain-usecases/Login.js
+++ domain-usecases/Login.js
@@ -3,33 +3,30 @@ const bcrypt = require("bcrypt");
 
 async function login(userName, password){
     
-    try {
-        // // regression test 1
-        // return "regression test try1";
-        // check fields exists
-        if(!userName || !password){
-            return "Missing fields, make sure you entered the following: userName, password.";
-        }
 
-        // check if user exists 
-        let exists = await soccerDB.findUserByUserName(userName);
-        if(!exists){
-            console.log('User not exists in the DB');
-            return "User not exists in the DB.";
-        }
-
-        // getting the password that in the DB and compare it to the password inserted by the user
-        let passwordDB = await soccerDB.getPasswordByUserName(userName);
-        if(!bcrypt.compareSync(password, passwordDB)){
-            console.log("Username or Password incorrect");
-            return "Username or Password incorrect";
-        }else{
-            return "200, User login successfully.";
-        }
-        
-    } catch (error) {
-        
+    // // regression test 1
+    // return "regression test try1";
+    // check fields exists
+    if(!userName || !password){
+        return "Missing fields, make sure you entered the following: userName, password.";
     }
+
+    // check if user exists 
+    let exists = await soccerDB.findUserByUserName(userName);
+    if(!exists){
+        console.log('User not exists in the DB');
+        return "User not exists in the DB.";
+    }
+
+    // getting the password that in the DB and compare it to the password inserted by the user
+    let passwordDB = await soccerDB.getPasswordByUserName(userName);
+    if(!bcrypt.compareSync(password, passwordDB)){
+        console.log("Username or Password incorrect");
+        return "Username or Password incorrect";
+    }else{
+        return "200, User login successfully.";
+    }
+        
 }
 
 exports.login = login;
